f98408693c82b4b62e8c595d32bb70c5140262c0
pixelcrisis <pxcrisis@gmail.com>
2024-12-23 22:21:26 -0700

    Replace Selects with Checkbox

--- src/views/FindView.vue
+++ src/views/FindView.vue
@@ -1,9 +1,15 @@
 <script setup>
+  import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
   <div class="wrap">
-    Find
+    <header>
+      <label class="input input-bordered input-primary flex items-center gap-2">
+        <input type="text" class="grow" placeholder="Search" />
+        <MagnifyingGlassIcon class="size-5" />
+      </label>
+    </header>
   </div>
 </template>
 
