78acb1a20d4dbc532dc1d73a09131c8b991ff711
pixelcrisis <pxcrisis@gmail.com>
2025-05-29 11:29:12 -0600

    Add empty list text/link.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,9 @@
   import PageWrap from '@/partials/PageWrap.vue'
   import DataWrap from '@/partials/DataWrap.vue'
 
+  import { RouterLink } from 'vue-router'
   import { useLibrary } from '@/stores/library'
-  import { QueueListIcon } from '@heroicons/vue/24/outline'
+  import { QueueListIcon, SquaresPlusIcon } from '@heroicons/vue/24/outline'
 
   const library = useLibrary()
 </script>
@@ -12,6 +13,21 @@
   <PageWrap :title="library.current.name" :icon="QueueListIcon" link="/listing">
 
     <DataWrap v-for="w in library.current.list" :data="w" />
+
+    <div class="text-center" v-if="!library.current.list.length">
+      <h3>I'm empty!</h3>
+      <router-link to="/search">
+        <SquaresPlusIcon class="size-20 inline-block" />
+        <br /> Find Something...
+      </router-link>
+      
+    </div>
     
   </PageWrap>
 </template>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+</style>
